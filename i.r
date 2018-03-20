e9e34b06b877b6c98e401bcea5fac33fb25d7f3e
Reza Norouzian <rnorouzian@gmail.com>
2018-03-20 00:23:04 -0500

    Update i.r

--- i.r
+++ i.r
@@ -2655,7 +2655,10 @@ standard <- function(data = mtcars, center = TRUE, scale = TRUE, na.rm = TRUE)
 
 standard.default <- function(data = mtcars, scale = TRUE, center = TRUE, na.rm = TRUE){
     
-  
+ 
+message("Note: You now have new column(s) in your 'data' with suffix '.s' ('.s' for standardized).\n
+         Also, 'NA's are cleaned by default. Use 'na.rm = FALSE' for otherwise.") 
+    
   if(inherits(data, "data.frame") && ncol(data) > 1){ 
     
     data <- if(na.rm) data[complete.cases(data), ]    
@@ -2664,7 +2667,6 @@ standard.default <- function(data = mtcars, scale = TRUE, center = TRUE, na.rm =
     
     data[paste0(names(data)[ind], ".s")] <- lapply(data[ind], scale)
     
-    message("Note: You now have new column(s) in your 'data' with suffix '.s' ('.s' for standardized).")
     return(data)
   }
   
