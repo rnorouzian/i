89449f5ca992ab8c2716769d772f99df1daf44ba
Reza Norouzian <rnorouzian@gmail.com>
2018-04-15 11:45:15 -0500

    Update i.r

--- i.r
+++ i.r
@@ -2352,13 +2352,13 @@ lm.cond.case.default <- function(fit, predi, scale = .5, level = .95, ...){
 #======================================================================================                       
 
                        
-predict.bayes <- function(fit, xlab = NA, ylab = NA, level = .95, line.int = TRUE, pred.int = TRUE, pt.cex = 1, pt.col = 4, col.depth = .3, ...)
+predict.bayes <- function(fit, xlab = NA, ylab = NA, level = .95, line.int = TRUE, pred.int = TRUE, pt.cex = 1, pt.col = 4, col.depth = .3, col.line = "cyan", col.pred = "gray", col.reg = "cyan", ...)
 {
   UseMethod("predict.bayes")
 } 
 
                        
-predict.bayes.default <- function(fit, xlab = NA, ylab = NA, level = .95, line.int = TRUE, pred.int = TRUE, pt.cex = 1, pt.col = 4, col.depth = .3, ...){
+predict.bayes.default <- function(fit, xlab = NA, ylab = NA, level = .95, line.int = TRUE, pred.int = TRUE, pt.cex = 1, pt.col = 4, col.depth = .3, col.line = "cyan", col.pred = "gray", col.reg = "cyan", ...){
 
 if(class(fit)[1] != "stanreg") stop("Error: 'fit' must be from package 'rstanarm's 'stan_glm()'.")  
 if(length(coef(fit)) > 2) stop("Error: 'fit' must contain only 'one' predictor.")
@@ -2389,7 +2389,7 @@ if(pred.int){
 y <- I[,1][o]
 z <- I[,2][o]
 
-polygon(c(rev(x), x), c(rev(z), y), col = adjustcolor('gray', .5), border = NA)
+polygon(c(rev(x), x), c(rev(z), y), col = adjustcolor(col.pred, .5), border = NA)
 }
     
 pred_lin2 <- rstanarm::posterior_linpred(fit, transform = TRUE)
@@ -2404,10 +2404,10 @@ if(line.int){
 y <- I2[,1][o]
 z <- I2[,2][o]
 
-polygon(c(rev(x), x), c(rev(z), y), col = adjustcolor('cyan', col.depth), border = NA)
+polygon(c(rev(x), x), c(rev(z), y), col = adjustcolor(col.line, col.depth), border = NA)
 }
     
-abline(fit, col = "cyan", lwd = 2)
+abline(fit, col = col.reg, lwd = 2)
 box()    
 }                       
                        
