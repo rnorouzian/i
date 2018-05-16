e9b2e7a807290761681e4dc7e1591efaa47c7147
Reza Norouzian <rnorouzian@gmail.com>
2018-05-16 11:32:38 -0500

    Update i.r

--- i.r
+++ i.r
@@ -3423,8 +3423,8 @@ anova.es.default <- function(fit = NA, f, df1, df2, N, conf.level = .9, digits =
   pomega <- (df1 * (f - 1)) / ((df1 * (f - 1)) + N)
   peta <- peta.ci(f = f, df1 = df1, df2 = df2, N = N, conf.level = conf.level, digits = digits)
   
-  res <- round(data.frame(eta.sq = eta, P.eta.sq = peta[,1], lowerP.eta.sq = peta[,2], 
-                          upperP.eta.sq = peta[,3], conf.level = peta[,4], omega.sq = omega, 
+  res <- round(data.frame(eta.sq = eta, P.eta.sq = peta[,1], lower.P.eta.sq = peta[,2], 
+                          upper.P.eta.sq = peta[,3], conf.level = peta[,4], omega.sq = omega, 
                           P.omega.sq = pomega), digits = digits)
   
   if(is.na(fit)){  
