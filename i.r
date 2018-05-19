1e850a5f595b68caf33e549fbdfe4bb5af538299
Reza Norouzian <rnorouzian@gmail.com>
2018-05-19 11:53:31 -0500

    Update i.r

--- i.r
+++ i.r
@@ -3406,7 +3406,7 @@ anova.es <- function(fit = NA, f, df1, df2, N, conf.level = .9, digits = 6)
                 
 anova.es.default <- function(fit = NA, f, df1, df2, N, conf.level = .9, digits = 6){
   
-  if(!is.na(fit)){
+  if(!(any(is.na(fit)))){
     
     if(fit$call[1] != "lm()" && fit$call[1] != "aov()") { stop("Error: 'fit' must be a fitted model from base R's 'aov()' or 'lm()' commands.") }        
     N <- nobs(fit)
@@ -3432,7 +3432,7 @@ anova.es.default <- function(fit = NA, f, df1, df2, N, conf.level = .9, digits =
   
   message("Note: If analysis includes random-effects, carefully pick the right 'df2' to obtain correct 'P.eta- or P.omega-sq.'")
   
-  if(is.na(fit)){  
+  if(!(any(is.na(fit)))){  
     
     return(result)
     
