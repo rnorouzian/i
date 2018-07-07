c9a295671bb9d22695b849543732430a59abb3c4
Reza Norouzian <rnorouzian@gmail.com>
2018-07-07 01:26:52 -0500

    Update i.r

--- i.r
+++ i.r
@@ -289,6 +289,39 @@ peta.ci <- function(peta, f = NA, df1, df2, N, conf.level = .9, digits = 9){
   round(data.frame(t(ci(peta = peta, f = f, N = N, df1 = df1, df2 = df2, conf.level = conf.level))), digits = digits)
 }               
 
+#=================================================================================================================================
+                  
+                  
+peta.cib <- function(peta, f = NA, df1, df2, N, conf.level = .9, digits = 9){
+
+ci <- Vectorize(function(peta, f, N, df1, df2, conf.level){
+    
+  q <- ifelse(is.na(f), peta2F(peta, df1, df2), f) 
+  
+    alpha <- (1 - conf.level)/2
+    
+    u <- function (ncp, alpha, q, df1, df2) {
+      suppressWarnings(pf(q = q, df1 = df1, df2 = df2, ncp, lower.tail = FALSE)) - alpha
+    }
+    
+    I <- try(sapply(c(alpha, 1-alpha), 
+         function(x) uniroot(u, c(0, q+1e3), alpha = x, q = q, df1 = df1, df2 = df2)[[1]]), silent = TRUE)
+    
+    if(inherits(I, "try-error")) return(c(NaN, message("Warning: NaN produced. Are input values correct?")))
+    
+    I <- I[1:2] / (I[1:2] + N)
+    
+    P.eta.sq <- if(is.na(f)) peta else F2peta(f, df1, df2)
+    
+    return(c(P.eta.sq = P.eta.sq, lower = I[1], upper = I[2], conf.level = conf.level, ncp = peta2ncp(P.eta.sq, N), F.value = q))
+})
+
+peta <- if(missing(peta)) NA else peta
+
+round(data.frame(t(ci(peta = peta, f = f, N = N, df1 = df1, df2 = df2, conf.level = conf.level))), digits = digits)
+}
+             
+             
 #=================================================================================================================================                
                 
 cor.ci <- function(r, n, conf.level = .95, digits = 9)
