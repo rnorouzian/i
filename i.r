bb17f1f7b4cb7cb4093c15961b016f18a129115d
Reza Norouzian <rnorouzian@gmail.com>
2018-07-13 11:29:07 -0500

    Update i.r

--- i.r
+++ i.r
@@ -5105,10 +5105,11 @@ exp.pov <- function(P2, K, N, regress = TRUE)
 #====================================================================================================================================================================
             
 
-plan.f.ci <- function(peta = .2, design = 2 * 2, n.level = 2, n.covar = 0, conf.level = .9, width = .2, regress = FALSE, n.groups = 0, assure = .99){
+plan.f.ci <- function(peta = .2, design = 2 * 2, n.level = 2, n.covar = 0, conf.level = .9, width = .2, regress = FALSE,  pair.design = 0, assure = .99){
   
   if(any(conf.level >= 1) || any(conf.level <= 0) || any(assure >= 1) || any(assure <= 0)) stop("'conf.level' and 'assure' must be between '0' and '1'.", call. = FALSE)
-  
+  n.groups <- pair.design
+    
   G <- Vectorize(function(peta, conf.level, width, assure, design, n.level, n.covar, regress, n.groups){
     
     n.f <- function(peta, conf.level, width, assure, design, n.level, n.covar, regress, n.groups){
