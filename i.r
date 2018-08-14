f059791c9d01bf2667a050ac0c0aed72eb77bccf
Reza Norouzian <rnorouzian@gmail.com>
2018-08-13 21:09:38 -0500

    Update i.r

--- i.r
+++ i.r
@@ -5441,3 +5441,42 @@ return(N)
  rcor <- function(n, mean = 0, N){ 
   tanh(rnorm(n = n, mean = atanh(mean), sd = 1/sqrt(N - 3)))
 }
+
+#=======================================================================================================================================
+      
+      
+mrnorm <- function(n = 1, mu, sd, tol = 1e-6, random = TRUE)
+{
+ 
+  UseMethod("mrnorm") 
+  
+}
+
+
+mrnorm.default <- function(n = 1, mu, sd, tol = 1e-6, random = TRUE) 
+{
+  p <- length(mu)
+  if (!all(dim(sd) == c(p, p))) 
+    stop("incompatible arguments")
+  eS <- eigen(sd, symmetric = TRUE)
+  ev <- eS$values
+  if (!all(ev >= -tol * abs(ev[1L]))) 
+    stop("'sd' is not positive definite")
+  X <- matrix(rnorm(p * n), n)
+  if (!random) {
+    X <- scale(X, TRUE, FALSE)
+    X <- X %*% svd(X, nu = 0)$v
+    X <- scale(X, FALSE, TRUE)
+  }
+  X <- drop(mu) + eS$vectors %*% diag(sqrt(pmax(ev, 0)), p) %*% 
+    t(X)
+  nm <- names(mu)
+  if (is.null(nm) && !is.null(dn <- dimnames(sd))) 
+    nm <- dn[[1L]]
+  dimnames(X) <- list(nm, NULL)
+  if (n == 1) 
+    drop(X)
+  else t(X)
+}
+      
+
