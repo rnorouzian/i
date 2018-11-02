382201062635a9b53d7347ea12a5b3b15975677b
Reza Norouzian <rnorouzian@gmail.com>
2018-11-01 22:57:09 -0500

    Update i.r

--- i.r
+++ i.r
@@ -5034,14 +5034,15 @@ return(c(n1 = n1, n2 = n2))
              
              
              
-plan.t.ci <- function(d, conf.level = .95, width, base.rate = 1, paired = FALSE, assure = .99)
+plan.t.ci <- function(d, conf.level = .95, width, base.rate = 1, paired = FALSE, assure = .99, expect = FALSE)
 {
   UseMethod("plan.t.ci")
 }
 
 
-plan.t.ci.default <- function(d, conf.level = .95, width, base.rate = 1, paired = FALSE, assure = .99){
+plan.t.ci.default <- function(d, conf.level = .95, width, base.rate = 1, paired = FALSE, assure = .99, expect = FALSE){
   
+if(expect) assure <- .5    
 if(any(conf.level >= 1) || any(conf.level <= 0) || any(assure >= 1) || any(assure <= 0)) stop("'conf.level' and 'assure' must be between '0' and '1'.", call. = FALSE)
   
   G <- Vectorize(function(d, conf.level, width, base.rate, paired, assure){
