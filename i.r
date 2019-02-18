b4cabf598b102737e79fb2fdffc9cd73cf6a0b94
Reza Norouzian <rnorouzian@gmail.com>
2019-02-18 16:45:42 -0600

    Update i.r

--- i.r
+++ i.r
@@ -7516,7 +7516,7 @@ plot.rootogram <- function(x,
     abline(h = 0, col = border, lty = 3)
     lines(d$x, d$line,
           col = col, pch = pch, type = type, lty = lty, lwd = lwd)
-    legend("topright", c("Model", "Data"), pch = c(19, NA), lwd = 2, col = c(col, 1), bty = "n")
+    legend("topright", c("Model", "Data"), pch = c(19, 124), lwd = c(2, NA), col = c(col, 1), bty = "n")
   }
   
   ## draw plots
