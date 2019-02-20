e03f7e2b23d787e8dfad5e2ba6e8bc0fe62d3336
Reza Norouzian <rnorouzian@gmail.com>
2019-02-20 16:14:15 -0600

    Update i.r

--- i.r
+++ i.r
@@ -5794,95 +5794,86 @@ Anova <- function(eta.sq = .25, n = 5, min.score = 0, max.score = 25, coef = 1.2
       
       
 players <- function(Step = 16, n.Players = 16, n.Steps = 16, step.size = .5, adj = 3){
-
+  
   graphics.off()
   original_par = par(no.readonly = TRUE)
   on.exit(par(original_par))
-
+  
   par(mar = c(2.2, 1.8, 1.5, 1.8) );
   m = matrix( c(1, 1, 1, 1, 1, 1,   2, 2), nrow = 8, ncol = 1 );
   layout(m)
-
+  
   if(n.Players < 2) { n.Players = 2;
   message("\n\tYou can't have less then \"2 players\" on the field.")}
-
+  
   if(Step > n.Steps) { Step = n.Steps;
   message("\n\tYou can't take more steps than what you planned to.\n\tWe picked the maximum steps that you planned to.")}
-
+  
   if(Step < 0 || n.Steps < 1) { Step = 0; n.Steps = 1;
   message("\n\tYou can't have less than \"0\" steps.\n\tAlso, You can't have less than \"1\" as your total steps.")}
-
+  
   plot(-6:6, -6:6, type = "n", axes = F, ann = F)
-
+  
   axis(1, at = c(-6:-1, 1:6), font.axis = 2, cex.axis = 1.5 )
   axis(1, at = 0, font.axis = 2, cex.axis = 1.9, col.axis = 'red' )
-
+  
   par = par('usr')
   rect(par[1], par[3], par[2], par[4], col = 'darkseagreen1' )
-
+  
   points( 0, 0, cex = 7, pch = 20, col = 0)
   points( 0, 0, cex = 40, lwd = 5, col = 0)
   abline(v = 0, lwd = 10, col = 0)
-    
+  
   rect(-6, -6, 6, 6, lwd = 5, border = 0)
   rect(-6.5, -2, -5.5, 2, col = 'darkseagreen1', border = 0, lwd = 5)
   rect(rep(-6.5, 2), rep(-2, 2), rep(-5.5, 2), rep(2, 2), border = 0, density = 10, angle = c(180, 90), col = 0)
-
+  
   rect(6.5, -2, 5.5, 2, col = 'darkseagreen1', border = 0, lwd = 5)
   rect(rep(6.5, 2), rep(-2, 2), rep(5.5, 2), rep(2, 2), border = 0, density = 10, angle = c(180, 90), col = 0)
-
+  
   box(col = 'darkseagreen1')
-
+  
   points( c( rep(par[1]+.01, 2), rep(par[2]-.01, 2) ), rep( c(-2, 2), 2 ), cex = 3, pch = 20, col = 0 )
-
+  
   x <- rep(0, n.Players)                       ## Initial position of players
   y <- seq(from = -6, to = 6, len = n.Players) ## y-position for players
-
+  
   ## Sample movement of players:
   xStepsMx <- matrix(sample(c(-1, 1)*step.size, n.Players*n.Steps, replace = TRUE),
                      nrow = n.Players, ncol = n.Steps)
-
+  
   ## Position of players:
   xPosMx <- t(sapply(1:nrow(xStepsMx), function(ii) cumsum(xStepsMx[ii,]))) + x
-
+  
   positions = if (Step > 0){ xPosMx[,Step] } else {  x  }
-
+  
   segments(positions, y, 0, y, lty = 2, col = 'red')
   points(positions, y, cex = 7, lwd = 3, pch = 21, bg = "white")
   text(positions, y, 1:n.Players, font = 2, cex = 1.5)
-
+  
   if (Step == 0) {
-
+    
     plot(1, 1, ty = 'n', axes = F, ann = F)
-
+    
     text(1, 1, "Let players take at least \"1\" step on the field.", cex = 2.5, font = 2, col = 'red4')
-
+    
   } else {
     par( mar = c(3.5, 4, 2, 2.1), mgp = c(2, .5, 0) )
-
+    
     DENS = density(positions, adjust = adj, n = 1e4)
 
-    x.DENS = DENS$x
-    y.DENS = DENS$y
-
-    plot( DENS, col = 'red', lwd = 3, xaxt = "n", xlab = "Positions",
-         ylab = "Probability", font.axis = 2, font.lab = 2, xlim = c(-6, 6), main = NA, bty = 'n',
-         zero.line = F)
-
-    low.exterme = par('usr')[3]
-
-    x.DENS.2 = seq(-6, 6)
-    y.DENS.2 = approx(x.DENS, y.DENS, xout = x.DENS.2 )
-
-    points( y.DENS.2, col = 'blue', cex = 2, type = "h")
-
+    plot( table(positions)/length(positions), col = 2, lwd = 3, xaxt = "n", xlab = "Positions", lend = 1,
+          ylab = "Probability", font.axis = 2, font.lab = 2, xlim = c(-6, 6), main = NA, bty = 'n', yaxs = "i")
+    
+    lines(DENS, col = 4, lwd = 2)
+    
     polygon(DENS, col = rgb(0, 1, 1, .1), border = NA )
-
+    
     axis(1, at = seq(-6, 6, len = 13), xlab = 'positions', font = 2, cex.axis = 1.2 )
-
+    
     legend("topleft", legend = c(paste("Steps Taken = ", Step), paste("Players =", n.Players) ),
            bty="n", inset = c(-.02, .01), text.font=2, text.col = 'red4', cex = 1.5)
-    }
+  }
 }
                      
 #======================================================================================================================
