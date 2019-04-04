fdd60582de42b6ac94a5d30e9a35b6adce7813da
Reza Norouzian <rnorouzian@gmail.com>
2019-04-04 15:55:25 -0500

    Update i.r

--- i.r
+++ i.r
@@ -5012,7 +5012,7 @@ plan.f.ci.default <- function(pov, design = 2 * 2, f = NA, n.level = 2, n.pred =
   if(expect) assure <- .5
   regress <- if(!is.null(n.pred)) TRUE else FALSE
   if(regress) n.level <- n.pred
-  if(!is.na(d)) { pov <- d2peta(d = d, n1 = 300, n2 = 300) ; n.level <- 2 ;
+  if(!is.na(d)) { pov <- d2peta(d = d, n1 = 500, n2 = 500) ; n.level <- 2 ;
   message("\nNote: For 'pairwise' comparisons, 'total.N' is for '2' groups.") }
   if(!is.na(d) & is.na(width)) width <- d.width.meta(lower = lower, upper = upper)
   if(!is.na(d) & width >= .3) width <- .3
@@ -5095,7 +5095,7 @@ plan.f.ci.default <- function(pov, design = 2 * 2, f = NA, n.level = 2, n.pred =
     
     NN3 <- if(!(peta.max %inn% c(a$lower, a$upper))) NN1 else max(NN1, NN2)
     
-    max.w <- root(pov = peta, df1 = n$df1, N = NN3, df2 = NN3 - design - n.covar, conf.level = conf.level)$w
+    max.w <- round(root(pov = peta, df1 = n$df1, N = NN3, df2 = NN3 - design - n.covar, conf.level = conf.level)$w, 3)
     
     return(c(peta = peta, total.N = NN3, width = width, n.level = n.level, design = design, conf.level = conf.level, max.width = max.w))
     
