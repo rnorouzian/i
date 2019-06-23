bdb5f6f8e49fba0b20cc9f27c0c2bfcd7b362e2e
Reza Norouzian <rnorouzian@gmail.com>
2019-06-23 16:57:59 -0500

    Update i.r

--- i.r
+++ i.r
@@ -8396,7 +8396,7 @@ cfactor <- function(df) exp(lgamma(df/2)-log(sqrt(df/2)) - lgamma((df-1)/2))
 #===========================================================================================================================
 
                                                           
-sdif <- function(n = NULL, mpre = NULL, mpos = NULL, sdpre = NULL, sdpos = NULL, r = NULL, t = NULL, F1 = NULL,
+sdif2 <- function(n = NULL, mpre = NULL, mpos = NULL, sdpre = NULL, sdpos = NULL, r = NULL, t = NULL, F1 = NULL,
                  sdp = NULL){
   
   if(!is.null(r) & !is.null(sdpre) & !is.null(sdpos)) sqrt(sdpre^2+sdpos^2-2*r*sdpre*sdpos)
@@ -8407,8 +8407,19 @@ sdif <- function(n = NULL, mpre = NULL, mpos = NULL, sdpre = NULL, sdpos = NULL,
 
 #===========================================================================================================================
                                                                                                               
+sdif <- function(n = NA, mpre = NA, mpos = NA, sdpre = NA, sdpos = NA, r = NA, t = NA, F1 = NA,
+                 sdp = NA){
+  
+  ifelse(!is.na(r) & !is.na(sdpre) & !is.na(sdpos), sqrt(sdpre^2+sdpos^2-2*r*sdpre*sdpos),
+  ifelse(!is.na(n) & is.na(r) & !is.na(t) & !is.na(mpre) & !is.na(mpos), sqrt((n*(mpos - mpre)^2)/(ifelse(is.na(F1) & !is.na(t), t^2, ifelse(!is.na(F1) & is.na(t), F1, NA)))), 
+  ifelse(!is.na(r) & !is.na(sdp), sqrt(2*sdp^2*(1-r)), NA)))
+  
+}      
                                                                                                               
-rdif <- function(n = NULL, mpre = NULL, mpos = NULL, sdpre = NULL, sdpos = NULL, t = NULL, F1 = NULL, sdif = NULL, 
+#===========================================================================================================================
+                                                                                                              
+                                                                                                              
+rdif2 <- function(n = NULL, mpre = NULL, mpos = NULL, sdpre = NULL, sdpos = NULL, t = NULL, F1 = NULL, sdif = NULL, 
                  sdp = NULL) {
   
   sdif <- if(is.null(sdif))sdif(sdpre = sdpre, sdpos = sdpos, t = t, r = NULL, n = n, mpos = mpos, mpre = mpre, F1 = F1, sdp = sdp) else sdif
@@ -8416,7 +8427,18 @@ rdif <- function(n = NULL, mpre = NULL, mpos = NULL, sdpre = NULL, sdpos = NULL,
   else NA
 }
 
-      
+
+#===========================================================================================================================
+    
+    
+rdif <- function(n = NA, mpre = NA, mpos = NA, sdpre = NA, sdpos = NA, t = NA, F1 = NA, sdif = NA, 
+                 sdp = NA) {
+  
+  sdif <- ifelse(is.na(sdif), sdif(sdpre = sdpre, sdpos = sdpos, t = t, r = NA, n = n, mpos = mpos, mpre = mpre, F1 = F1, sdp = sdp), sdif)
+  ifelse(!is.na(sdif) & is.na(sdp) & !is.na(sdpre) & !is.na(sdpos), (sdpre^2 + sdpos^2 - sdif^2)/(2*sdpre*sdpos), ifelse(!is.na(sdp) & !is.na(sdif), 1 - (sdif^2/(2*sdp^2)), 
+   NA))
+}
+    
 #===========================================================================================================================
       
     
@@ -8486,7 +8508,7 @@ if(is.null(ll)) out else {
 #============================================================================================================================
                                       
                                       
-d.prepos <- function(n = NULL, mpre = NULL, mpos = NULL, sdpre = NULL, sdpos = NULL, r = NULL, autoreg = FALSE, t = NULL, sdif = NULL, sdp = NULL, F1 = NULL, df2 = NULL, d.per.study = NULL, extract, study.name = NULL, group.name = NULL, post, control, ...) 
+d.prepos4 <- function(n = NULL, mpre = NULL, mpos = NULL, sdpre = NULL, sdpos = NULL, r = NULL, autoreg = FALSE, t = NULL, sdif = NULL, sdp = NULL, F1 = NULL, df2 = NULL, d.per.study = NULL, extract, study.name = NULL, group.name = NULL, post, control, ...) 
 {
   
   if(missing(control) || missing(post)) stop("'post' or/and 'control' missing.", call. = FALSE)  
@@ -8665,7 +8687,7 @@ h
 #=====================================================================================================
 
             
-dint <- function(..., per.study, study.name = NULL, group.name = NULL, n.sim = 1e5, digits = 6, by = NULL)
+dint4 <- function(..., per.study, study.name = NULL, group.name = NULL, n.sim = 1e5, digits = 6, by = NULL)
   {
 
 L <- fuse(... = ..., per.study = per.study)
@@ -9067,7 +9089,7 @@ if(!Short & test[2]) list(SHORT = result1, LONG = result2) else list(SHORT = res
 #=====================================================================================================  
                
                
-meta.within <- function(..., per.study, study.name = NULL, group.name = NULL, outcome.name = NULL, tau.prior = function(x){dhnorm(x)}, by = NULL){
+meta.within3 <- function(..., per.study, study.name = NULL, group.name = NULL, outcome.name = NULL, tau.prior = function(x){dhnorm(x)}, by = NULL){
   
   L <- dint(... = ..., per.study = per.study, group.name = group.name, study.name = study.name, by = by)
   
@@ -9167,7 +9189,7 @@ meta.within <- function(..., per.study, study.name = NULL, group.name = NULL, ou
 #=====================================================================================================
 
               
-meta.bayes <- function(..., per.study, group.name = NULL, study.name = NULL, outcome.name = NULL, tau.prior = function(x){dhnorm(x)}, by = NULL, long = FALSE)
+meta.bayes3 <- function(..., per.study, group.name = NULL, study.name = NULL, outcome.name = NULL, tau.prior = function(x){dhnorm(x)}, by = NULL, long = FALSE)
 {
   
 
@@ -9261,6 +9283,323 @@ fill <- function(refdf, ...)
   c(list(refdf), res)
 }               
                
+#=====================================================================================================
+               
+d.prepos <- function(study.name = NA, group.name = NA, n = NA, mpre = NA, mpos = NA, sdpre = NA, sdpos = NA, r = NA, autoreg = FALSE, t = NA, sdif = NA, sdp = NA, F1 = NA, df2 = NA, post, control, ...) 
+{
+  
+  if(missing(control) || missing(post)) stop("'post' or/and 'control' missing.", call. = FALSE)  
+  
+  r <- ifelse(!is.logical(post) & autoreg & !is.na(r), autoreg(max(post, na.rm = TRUE), r)[,1][-1][post], r)
+  
+  d <- ifelse(!is.na(t) & !missing(n), t2d(t, n), ifelse(!is.na(F1) & !missing(n), t2d(sqrt(F1), n), ifelse(!is.na(F1) & missing(n) & !is.na(df2), t2d(sqrt(F1), df2+2), NA)))
+  mdif <- ifelse(!is.na(mpre) & !is.na(mpre), mpos - mpre, NA)
+  sdif <- ifelse(is.na(sdif), sdif(sdpre = sdpre, sdpos = sdpos, t = t, r = r, n = n, mpos = mpos, mpre = mpre, F1 = F1, sdp = sdp), sdif)
+  cor. <- ifelse(is.na(r), rdif(n = n, mpre = mpre, mpos = mpos, sdpre = sdpre, sdpos = sdpos, sdif = sdif, sdp = sdp), r)
+  d <- ifelse(!is.na(mdif) & is.na(d) & !is.na(sdif), mdif/sdif, d)*cfactor(n-1) 
+  
+  se <- se.d(d, n1 = n, g = TRUE)
+  
+out <- data.frame(d = d, SE = se, n = n, sdif = sdif, rpr.po = cor., post, control, ...)
+
+if(!anyNA(group.name) & length(group.name) == nrow(out)) row.names(out) <- as.character(group.name) else if(!anyNA(group.name) & length(group.name) != nrow(out)) stop("'group.name' incorrectly specified.", call. = FALSE)
+  
+if(all(is.na(out$d))) stop("\ninsufficient info. to calculate effect size(s).", call. = FALSE)
+  
+ return(out)
+ 
+}      
+ 
+
+#=====================================================================================================
+               
+               
+dint <- function(..., per.study = NULL, study.name = NA, group.name = NA, n.sim = 1e5, digits = 6, by, data = NULL)
+  {
+
+L <- if(!is.null(data)){
+  
+  m <- split(data, data$study.name) ; m[[1]] <- NULL
+  
+  dot.names <- names(m[[1]])[!names(m[[1]]) %in% formalArgs(d.prepos)]
+  
+  args <- lapply(m, function(x) unclass(x[c(head(formalArgs(d.prepos), -1), dot.names)]))
+  
+  argsT <- setNames(lapply(names(args[[1]]), 
+           function(i) lapply(args, `[[`, i)), names(args[[1]]))
+  
+do.call(Map, c(f = d.prepos, argsT))
+  
+} else { fuse(... = ..., per.study = per.study) }
+
+
+if(!missing(by)){
+  
+s <- substitute(by)    
+
+k <- as.list(s)
+
+if("control" %in% k || "!control" %in% k) stop("'control' can't be a moderating variable either alone or with other variables.", call. = FALSE)
+
+H <- lapply(L, function(x) do.call("subset", list(x, s)))
+
+res <- Filter(nrow, H)
+
+L <- if(length(res) == 0) stop("No study with the requested moderators found.", call. = FALSE) else res
+}
+  
+G <- function(m, study.name, group.name, n.sim, digits)
+  {
+  
+    cc <- reget(m, control, F)
+    
+    if(is.null(cc)) stop("Required 'control' group not found.", call. = FALSE)
+  
+    cdel1 <- reget(m, control & post == 2, F)
+    cdel2 <- reget(m, control & post == 3, F)
+    cs <- reget(m, control & post == 1, F)
+    
+    tdel1 <- reget(m, !control & post == 2, F)
+    tdel2 <- reget(m, !control & post == 3, F)
+    ts <- reget(m, !control & post == 1, F)  
+    
+    if(all(sapply(list(cdel1, cdel2, tdel1, tdel2, ts, cs), is.null))) stop("Either 'control' or 'post' incorrectly coded.", call. = FALSE)
+    
+    short <- all(sapply(list(cs, ts), function(x) !is.null(x)))
+    
+    del1 <- all(sapply(list(cdel1, tdel1), function(x) !is.null(x)))
+    
+    del2 <- all(sapply(list(cdel2, tdel2), function(x) !is.null(x)))
+    
+    
+    nc1 <- ncs <- m$n[m$control & m$post == 1]
+    nc2 <- ncdel1 <- m$n[m$control & m$post == 2]
+    nc3 <- ncdel2 <- m$n[m$control & m$post == 3]
+    
+    nt1 <- nts <- m$n[m$control == FALSE & m$post == 1]
+    nt2 <- ntdel1 <- m$n[m$control == FALSE & m$post == 2]
+    nt3 <- ntdel2 <- m$n[m$control == FALSE & m$post == 3]
+    
+    
+    if(short){
+      dps <- pair(cs, ts)  
+      dppc1 <- dppcs <- sapply(1:length(dps), function(i) dps[[i]][[1]][1])
+      dppt1 <- dppts <- sapply(1:length(dps), function(i) dps[[i]][[1]][2])
+      group.name1 <- unlist(lapply(1:length(dps), function(i) names(dps[[i]])))
+      SHORT <- data.frame(t(dit(dppc = dppc1, dppt = dppt1, nc = nc1, nt = nt1, n.sim = n.sim, digits = digits)))
+      row.names(SHORT) <- group.name1
+    }
+    
+    if(del1){
+      dpdel1 <- pair(cdel1, tdel1)
+      dppc2 <- dppcdel1 <- sapply(1:length(dpdel1), function(i) dpdel1[[i]][[1]][1])
+      dppt2 <- dpptdel1 <- sapply(1:length(dpdel1), function(i) dpdel1[[i]][[1]][2])
+      group.name2 <- unlist(lapply(1:length(dpdel1), function(i) names(dpdel1[[i]])))
+      DEL1 <- data.frame(t(dit(dppc = dppc2, dppt = dppt2, nc = nc2, nt = nt2, n.sim = n.sim, digits = digits)))
+      row.names(DEL1) <- group.name2
+    }
+    
+    if(del2){
+      dpdel2 <- pair(cdel2, tdel2)
+      dppc3 <- dppcdel2 <- sapply(1:length(dpdel2), function(i) dpdel2[[i]][[1]][1])
+      dppt3 <- dpptdel2 <- sapply(1:length(dpdel2), function(i) dpdel2[[i]][[1]][2])
+      group.name3 <- unlist(lapply(1:length(dpdel2), function(i) names(dpdel2[[i]])))
+      DEL2 <- data.frame(t(dit(dppc = dppc3, dppt = dppt3, nc = nc3, nt = nt3, n.sim = n.sim, digits = digits)))
+      row.names(DEL2) <- group.name3
+    }
+    
+    
+    list(SHORT = if(short) SHORT else NULL, DEL1 = if(del1) DEL1 else NULL, DEL2 = if(del2) DEL2 else NULL) 
+}
+
+h <- lapply(1:length(L), function(i) G(m = L[[i]], study.name = study.name, group.name = group.name, n.sim = n.sim, digits = digits))
+
+if(!is.null(data)) study.name <- names(L)
+
+names(h) <- if(anyNA(study.name)) paste0("Study", seq_along(h)) else if(!anyNA(study.name) & length(study.name) == length(h)) study.name else if(!anyNA(study.name) & length(study.name) != length(h)) stop("'study.name' incorrectly specified.", call. = FALSE)
+return(h)
+
+}               
+
+    
+#=====================================================================================================
+            
+            
+meta.within <- function(..., per.study = NULL, study.name = NA, group.name = NA, outcome.name = NA, tau.prior = function(x){dhnorm(x)}, by, data = NULL){
+  
+  L <- eval(substitute(dint(... = ..., per.study = per.study, group.name = group.name, study.name = study.name, by = by, data = data)))
+  
+  outcome.name <- if(anyNA(outcome.name)) " " else paste0(outcome.name, ":")
+  
+  if(!is.null(data)) study.name <- names(L)
+  
+  G <- function(object, study.name, tau.prior, outcome.name)
+  {
+    
+    d1 <- object$SHORT$dint
+    sd1 <- object$SHORT$SD
+    
+    d2 <- object$DEL1$dint
+    sd2 <- object$DEL1$SD
+    
+    d3 <- object$DEL2$dint
+    sd3 <- object$DEL2$SD
+    
+    
+    Short <- all(sapply(list(d1), function(x) !is.null(x)))
+    
+    Del1 <- all(sapply(list(d2), function(x) !is.null(x)))
+    
+    Del2 <- all(sapply(list(d3), function(x) !is.null(x)))
+    
+    
+    if(Short & length(d1) == 1) { 
+      
+      out <- data.frame(Mean.dint.short = d1, SD.dint.short = sd1)
+             rownames(out) <- outcome.name
+             return(out)
+    }
+    
+    
+    if(Del1 & length(d2) == 1) { 
+      
+      out <- data.frame(Mean.dint.del1 = d2, SD.dint.del1 = sd2)
+      rownames(out) <- outcome.name
+      return(out)
+    }
+    
+    
+    if(Del2 & length(d3) == 1) { 
+      
+      out <- data.frame(Mean.dint.del2 = d3, SD.dint.del2 = sd3)
+      rownames(out) <- outcome.name
+      return(out)
+    }
+    
+    
+    if(Short & length(d1) > 1){
+    result1 <- bayesmeta(     y = d1,
+                              sigma = sd1,
+                              labels = NULL, tau.prior = tau.prior)
+    result1$call <- match.call(expand.dots = FALSE)
+    
+    short <- c(result1$summary["mean","mu"], result1$summary["sd","mu"])
+    
+    }
+    
+    
+    if(Del1 & length(d2) > 1) {
+      result2 <- bayesmeta(     y = d2,
+                                sigma = sd2,
+                                labels = NULL, tau.prior = tau.prior)
+     result2$call <- match.call(expand.dots = FALSE)
+     
+     del1 <- c(result2$summary["mean","mu"], result2$summary["sd","mu"])
+    }
+    
+    
+    if(Del2 & length(d3) > 1) {
+      result3 <- bayesmeta(     y = d3,
+                                sigma = sd3,
+                                labels = NULL, tau.prior = tau.prior)
+      result3$call <- match.call(expand.dots = FALSE)
+      
+      del2 <- c(result3$summary["mean","mu"], result3$summary["sd","mu"])
+    }
+    
+    
+    out <- data.frame(Mean.dint.short = if(Short)short[1] else NA, SD.dint.short = if(Short) short[2]else NA, Mean.dint.del1 = if(Del1)del1[1]else NA, SD.dint.del1 = if(Del1)del1[2]else NA, Mean.dint.del2 = if(Del2)del2[1]else NA, SD.dint.del2 = if(Del2)del2[2]else NA) 
+    rownames(out) <- outcome.name
+    return(out)
+  }             
+  
+  h <- lapply(1:length(L), function(i) G(object = L[[i]], study.name = study.name, outcome.name = outcome.name, tau.prior = tau.prior))
+  names(h) <- study.name
+  return(h)
+}
+ 
+              
+#=====================================================================================================
+              
+              
+meta.bayes <- function(..., per.study = NULL, group.name = NA, study.name = NA, outcome.name = NA, tau.prior = function(x){dhnorm(x)}, by, long = FALSE, data = NULL)
+{
+  
+j <- eval(substitute(meta.within(... = ..., per.study = per.study, group.name = group.name, study.name = study.name, outcome.name = outcome.name, tau.prior = tau.prior, by = by, data = data)))
+  
+if(!is.null(data)) study.name <- names(j)
+
+d1 <- unlist(lapply(1:length(j), function(i) j[[i]]$Mean.dint.short))
+
+sd1 <- unlist(lapply(1:length(j), function(i) j[[i]]$SD.dint.short))
+
+
+d2 <- unlist(lapply(1:length(j), function(i) j[[i]]$Mean.dint.del1))
+
+sd2 <- unlist(lapply(1:length(j), function(i) j[[i]]$SD.dint.del1))
+
+
+d3 <- unlist(lapply(1:length(j), function(i) j[[i]]$Mean.dint.del2))
+
+sd3 <- unlist(lapply(1:length(j), function(i) j[[i]]$SD.dint.del2))
+
+
+test <- sapply(list(d1, d2, d3), function(x) length(x) >= 2 & all(!is.na(x)))
+
+
+if(all(!test)) stop("Insufficient studies to meta-analyze either 'short-' or 'long-term' effects.", call. = FALSE)
+
+
+if(test[1]) { result1 <- bayesmeta(     y = d1,
+                                    sigma = sd1,
+                                   labels = study.name, tau.prior = tau.prior)
+   result1$call <- match.call(expand.dots = FALSE)
+} 
+  
+
+if(test[2]) { result2 <- bayesmeta(     y = d2,
+                                    sigma = sd2,
+                                   labels = study.name, tau.prior = tau.prior)
+   result2$call <- match.call(expand.dots = FALSE)
+}  
+  
+
+if(test[3]) { result3 <- bayesmeta(     y = d3,
+                                    sigma = sd3,
+                                   labels = study.name, tau.prior = tau.prior)
+   result3$call <- match.call(expand.dots = FALSE)
+}  
+
+
+if(test[2] & test[3] & long){
+  
+ddelys <- c(result2$summary["mean","mu"], result3$summary["mean","mu"])
+sdelys <- c(result2$summary["sd","mu"], result3$summary["sd","mu"])
+
+       result4 <- bayesmeta(     y = ddelys,
+                             sigma = sdelys,
+                            labels = c("Delay1", "Delay2"), tau.prior = tau.prior)
+   result4$call <- match.call(expand.dots = FALSE)
+
+   if(test[1])return(list(SHORT = result1, LONG = result4))
+   if(!test[1])return(list(LONG = result4))
+}
+
+if(!test[1]) message("NOTE: No or insufficient studies to meta-analyze 'short-term' effects.")
+if(!test[2]) message("NOTE: No or insufficient studies to meta-analyze 'delayed 1' effects.")
+if(!test[3]) message("NOTE: No or insufficient studies to meta-analyze 'delayed 2' effects.")
+
+
+if(!long || long & !test[2] || long & !test[3]){ 
+
+if(all(test)) return(list(SHORT = result1, DEL1 = result2, DEL2 = result3))
+if(test[1] & test[2] & !test[3]) return(list(SHORT = result1, DEL1 = result2))
+if(test[1] & !test[2] & !test[3]) return(list(SHORT = result1))
+if(!test[1] & test[2] & !test[3]) return(list(DEL1 = result2))
+if(!test[1] & !test[2] & test[3]) return(list(DEL2 = result3))
+if(!test[1] & test[2] & test[3]) return(list(DEL1 = result2, DEL2 = result3))
+   }             
+}
                
 #=====================================================================================================          
              
