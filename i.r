3d0a78872e3139383c411e1eed7ca2daa225c00d
Reza Norouzian <rnorouzian@gmail.com>
2019-12-06 15:11:01 -0600

    Update i.r

--- i.r
+++ i.r
@@ -21,9 +21,9 @@ message(Break, notice, Break)
 
 Break = "\n*****************************************************************************\n"
 
-cite = "To cite the package use:\n\nNorouzian, R., de Miranda, M. A., & Plonsky, L. (2018). The Bayesian \nrevolution in second language research: An applied approach. Language Learning, 64, 1032-1075.
-
-\nNorouzian, R., de Miranda, M. A., & Plonsky, L. (2019). A Bayesian approach to measuring evidence \nin L2 research: An empirical investigation. Modern Language Journal, 103, 248-263."
+cite <- "To cite the package use:\n\nNorouzian, R., de Miranda, M. A., & Plonsky, L. (2018). The Bayesian \nrevolution in second language research: An applied approach. Language Learning, 64, 1032-1075.
+\nNorouzian, R., de Miranda, M. A., & Plonsky, L. (2019). A Bayesian approach to measuring evidence \nin L2 research: An empirical investigation. Modern Language Journal, 103, 248-263.
+\nNorouzian, R. (in press). Sample size planning in quantitative L2 research: A pragmatic approach. \nStudies in Second Language Acquisition."
 
 cat(Break, cite, Break)
 
