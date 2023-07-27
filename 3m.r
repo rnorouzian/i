5ebf32545b1a689459008220d81b459b09ca84e3
Reza Norouzian <rnorouzian@gmail.com>
2023-07-26 21:22:06 -0500

    Update 3m.r

--- 3m.r
+++ 3m.r
@@ -1802,6 +1802,9 @@ pct_dif_tran <- list(
 
 coef.post_rma <- function(post_rma_fit, none_names = NULL, ..., sep = get_emm_option("sep")){
   
+  if(!inherits(post_rma_fit, "post_rma"))
+    stop("post_rma_fit can be either from 'post_rma()' or 'contrast_rma()'.", call. = FALSE)
+  
   ems <- post_rma_fit$ems
   
   disp <- if(is.null(ems@misc$display)) seq_len(nrow(ems@linfct)) else ems@misc$display
@@ -1815,33 +1818,88 @@ coef.post_rma <- function(post_rma_fit, none_names = NULL, ..., sep = get_emm_op
 
 #================================================================================================================================================
 
-vcov.post_rma <- function(post_rma_fit, ..., sep = get_emm_option("sep")){
-
-object <- post_rma_fit$ems  
+coef.contrast_rma <- function(post_rma_fit, none_names = NULL, ..., sep = get_emm_option("sep")){
   
-vcov.emmGrid = function(object, ..., sep) {
+  if(!inherits(post_rma_fit, "contrast_rma"))
+    stop("post_rma_fit can be either from 'post_rma()' or 'contrast_rma()'.", call. = FALSE)
+  
+  ems <- post_rma_fit$con
+  
+  disp <- if(is.null(ems@misc$display)) seq_len(nrow(ems@linfct)) else ems@misc$display
+  
+  largs <- as.list(ems@grid[disp, seq_along(ems@levels), drop = FALSE])
+  largs$sep <- sep
+  Term <- do.call(paste, largs)
+  
+  setNames(predict(ems), Term) 
+} 
+
+#================================================================================================================================================
+
+vcov.post_rma <- function(post_rma_fit, ..., sep = get_emm_option("sep")){
+  
+  if(!inherits(post_rma_fit, "post_rma"))
+      stop("post_rma_fit can be either from 'post_rma()' or 'contrast_rma()'.", call. = FALSE)
+  
+  object <- post_rma_fit$ems
+  
+  vcov.emmGrid = function(object, ..., sep) {
     tol = get_emm_option("estble.tol")
     if (!is.null(hook <- object@misc$vcovHook)) {
-        if (is.character(hook)) 
-            hook = get(hook)
-        hook(object, tol = tol, ...)
+      if (is.character(hook)) 
+        hook = get(hook)
+      hook(object, tol = tol, ...)
     }
     else {
-        if(is.null(disp <- object@misc$display))
-            disp = seq_len(nrow(object@linfct))
-        X = object@linfct[disp, , drop = FALSE]
-        estble = estimability::is.estble(X, object@nbasis, tol) 
-        X[!estble, ] = NA
-        X = X[, !is.na(object@bhat), drop = FALSE]
-        rtn = X %*% tcrossprod(object@V, X)
-        largs = as.list(object@grid[disp, seq_along(object@levels), drop = FALSE])
-        largs$sep = sep
-        rownames(rtn) = colnames(rtn) = do.call(paste, largs)
-        return(rtn)
+      if(is.null(disp <- object@misc$display))
+        disp = seq_len(nrow(object@linfct))
+      X = object@linfct[disp, , drop = FALSE]
+      estble = estimability::is.estble(X, object@nbasis, tol) 
+      X[!estble, ] = NA
+      X = X[, !is.na(object@bhat), drop = FALSE]
+      rtn = X %*% tcrossprod(object@V, X)
+      largs = as.list(object@grid[disp, seq_along(object@levels), drop = FALSE])
+      largs$sep = sep
+      rownames(rtn) = colnames(rtn) = do.call(paste, largs)
+      return(rtn)
     }
-}
+  }
+  
+  vcov.emmGrid(object = object, ..., sep = sep)
+}        
 
-vcov.emmGrid(object = object, ..., sep = sep)
+#================================================================================================================================================
+
+vcov.contrast_rma <- function(post_rma_fit, ..., sep = get_emm_option("sep")){
+  
+  if(!inherits(post_rma_fit, "contrast_rma"))
+    stop("post_rma_fit can be either from 'post_rma()' or 'contrast_rma()'.", call. = FALSE)
+  
+  object <- post_rma_fit$con
+  
+  vcov.emmGrid = function(object, ..., sep) {
+    tol = get_emm_option("estble.tol")
+    if (!is.null(hook <- object@misc$vcovHook)) {
+      if (is.character(hook)) 
+        hook = get(hook)
+      hook(object, tol = tol, ...)
+    }
+    else {
+      if(is.null(disp <- object@misc$display))
+        disp = seq_len(nrow(object@linfct))
+      X = object@linfct[disp, , drop = FALSE]
+      estble = estimability::is.estble(X, object@nbasis, tol) 
+      X[!estble, ] = NA
+      X = X[, !is.na(object@bhat), drop = FALSE]
+      rtn = X %*% tcrossprod(object@V, X)
+      largs = as.list(object@grid[disp, seq_along(object@levels), drop = FALSE])
+      largs$sep = sep
+      rownames(rtn) = colnames(rtn) = do.call(paste, largs)
+      return(rtn)
+    }
+  }
+  
+  vcov.emmGrid(object = object, ..., sep = sep)
 }        
         
 #======================== WCF Meta Dataset ======================================================================================================                
