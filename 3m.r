817ec5628abd56fd678d4f19d146a3bf6d4cf595
Reza Norouzian <rnorouzian@gmail.com>
2023-07-26 22:26:37 -0500

    Update 3m.r

--- 3m.r
+++ 3m.r
@@ -1445,7 +1445,7 @@ contr_rma <- function(post_rma_fit, contr_index){
 
 #===================================================================================================================================================
 
-prob_rma <- function(post_rma_fit, target_effect = 0, condition = c("or larger", "or smaller"), gain = FALSE, none_names = NULL, ...){
+prob_rma <- function(post_rma_fit, target_effect = 0, condition = c("or larger", "or smaller"), gain = FALSE, ..., sep = get_emm_option("sep")){
   
   
   if(!inherits(post_rma_fit, c("post_rma","contrast_rma"))) stop("post_rma_fit is not 'post_rma()' or 'contrast_rma()'.", call. = FALSE)   
@@ -1456,20 +1456,18 @@ prob_rma <- function(post_rma_fit, target_effect = 0, condition = c("or larger",
   
   specs <- post_rma_fit$specs
   
+  digits <- post_rma_fit$digits
+  
+  ems <- if(inherits(post_rma_fit, "post_rma")) post_rma_fit$ems else 
+    post_rma_fit$con
+  
   post_rma_fit <- type.convert(post_rma_fit$table, as.is=TRUE)
   
-  nms <- names(post_rma_fit)
+  disp <- if(is.null(ems@misc$display)) seq_len(nrow(ems@linfct)) else ems@misc$display
   
-  contr <- if(any("contr" %in% as.character(post_rma_fit$call)) || any(c("pairwise","revpairwise","tukey","consec",
-                                                                         "poly","trt.vs.ctrl","trt.vs.ctrlk","trt.vs.ctrl1",
-                                                                         "dunnett","mean_chg","eff","del.eff","identity") %in% as.character(specs))) "Contrast" else NULL
-  
-  vv <- nms[!nms %in% c("Mean","Response","SE","Df","Lower","Upper","t",      
-                        "p-value","Sig.",contr,"F","Df1","Df2",
-                        "Estimate","Term","Block Contrast","(M)UTOS Term", none_names)]
-  
-  Term <-sapply(seq_len(nrow(post_rma_fit)), 
-                function(i) paste0(as.vector(unlist(post_rma_fit[vv][i,])), collapse = " "))
+  largs <- as.list(ems@grid[disp, seq_along(ems@levels), drop = FALSE])
+  largs$sep <- sep
+  Term <- do.call(paste, largs)
   
   
   cond <- match.arg(condition)  
@@ -1482,7 +1480,7 @@ prob_rma <- function(post_rma_fit, target_effect = 0, condition = c("or larger",
   
   upper_ave_eff <- post_rma_fit$Upper
   
-  ave_eff <- if(!is.null(post_rma_fit$Mean)) post_rma_fit$Mean else if(!is.null(post_rma_fit$Response)) post_rma_fit$Response else post_rma_fit$Estimate
+  ave_eff <- round(predict(ems), digits = digits)
   
   ci <- as.data.frame(confint.rma.mv(fit, ...))
   
@@ -1502,8 +1500,7 @@ prob_rma <- function(post_rma_fit, target_effect = 0, condition = c("or larger",
   
   data.frame(Term=Term, Target_Effect = paste(target_effect, cond, collapse = " "), Probability = Probability, 
              Min = min_Probability, Max = max_Probability)
-  
-}
+} 
 
 #M==============================================================================================================================================
 
