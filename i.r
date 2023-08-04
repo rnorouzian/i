962e5a973f652de7d5551c93353f055d7adc9988
Reza Norouzian <rnorouzian@gmail.com>
2023-08-04 09:44:53 -0500

    Update i.r

--- i.r
+++ i.r
@@ -4945,14 +4945,12 @@ plan.t.ci.default <- function(d, t = NA, n1, n2 = NA, conf.level = .95, width = 
   if(is.na(width)) width <- d.width(d = d, t = t, n1 = n1, n2 = n2, conf.level = conf.level)
   if(expect) assure <- .5
   
-  inc <- if(is.character(increase.by)) as.numeric(substr(increase.by, 1, nchar(increase.by)-1))/ 1e2 else increase.by
-  red <- if(is.character(reduce.by)) as.numeric(substr(reduce.by, 1, nchar(reduce.by)-1))/ 1e2 else reduce.by
+inc <- ifelse(is.character(increase.by), as.numeric(substr(increase.by, 1, nchar(increase.by)-1))/ 1e2, increase.by)
+red <- ifelse(is.character(reduce.by), as.numeric(substr(reduce.by, 1, nchar(reduce.by)-1))/ 1e2, reduce.by)
   
-fac <- if(all(inc != 0 & red == 0)) { 1 + inc
-} else if(all(red != 0 & inc == 0)) { 1 - red 
-} else { 1 }
-  
-  
+fac <- ifelse(inc != 0 & red == 0, 1 + inc, 
+              ifelse(red != 0 & inc == 0, 1 - red, 1))
+    
 fac[fac <= 0] <- 1
 fac[inc == 0 & fac > 1] <- 1
   
@@ -5046,12 +5044,11 @@ pov <- ifelse(!is.na(d) & pov <= .15,  .15, pov)
     
   peta <- pov
   
-  inc <- if(is.character(increase.by)) as.numeric(substr(increase.by, 1, nchar(increase.by)-1))/ 1e2 else increase.by
-  red <- if(is.character(reduce.by)) as.numeric(substr(reduce.by, 1, nchar(reduce.by)-1))/ 1e2 else reduce.by
+inc <- ifelse(is.character(increase.by), as.numeric(substr(increase.by, 1, nchar(increase.by)-1))/ 1e2, increase.by)
+red <- ifelse(is.character(reduce.by), as.numeric(substr(reduce.by, 1, nchar(reduce.by)-1))/ 1e2, reduce.by)
   
-fac <- if(all(inc != 0 & red == 0)) { 1 + inc
-} else if(all(red != 0 & inc == 0)) { 1 - red 
-} else { 1 }
+fac <- ifelse(inc != 0 & red == 0, 1 + inc, 
+              ifelse(red != 0 & inc == 0, 1 - red, 1))
   
 fac[fac <= 0] <- 1
 fac[inc == 0 & fac > 1] <- 1
