ff296010b34f8514a4fca9c55eb1712e42164807
Reza Norouzian <rnorouzian@gmail.com>
2023-11-19 22:09:45 -0600

    Update 3m.r

--- 3m.r
+++ 3m.r
@@ -2255,8 +2255,8 @@ plot_rma <- function(fit, formula, ylab, CIs=TRUE, PIs=FALSE,
   
   if(missing(ylab)) ylab <- paste0("Effect Size (",as.character(fixed_form_rma(if(is_post_rma) fit$rma.mv_fit else fit))[2],")")
   
-  if(no_ciarg & CIs & interpolate) CIarg <- list(lwd = 3, alpha = 0.03)
-  if(no_piarg & PIs & interpolate) PIarg <- list(lwd = 3, alpha = 0.03)
+  if(no_ciarg & CIs & interpolate) CIarg <- list(lwd = 3, alpha = 0.04)
+  if(no_piarg & PIs & interpolate) PIarg <- list(lwd = 3, alpha = 0.04)
   if(no_linearg & CIs & interpolate||no_linearg & PIs & interpolate) linearg <- list(size=1,alpha=1,linetype = "solid")
   
   fit <- if(!is_post_rma) { 
