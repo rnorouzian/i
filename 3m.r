b40d26bff236431e258fa31cb1c4a7e89106d1e7
Reza Norouzian <rnorouzian@gmail.com>
2024-02-21 11:52:58 -0600

    Update 3m.r

--- 3m.r
+++ 3m.r
@@ -2465,7 +2465,49 @@ r2z_tran <- list(
     all(is.finite(eta)) && all(abs(eta) <= 1),
   name = "r2z"
 )                                        
-        
+# M=================================================================================================================================================
+
+con_gain_list <- function(post_rma_fit, pretest_name = "baseline", 
+         posttest_base_name = "posttest", dif_gain = FALSE,
+         posttest_suffix = "\\d+", sep = get_emm_option("sep")){
+
+tms <- unlist(strsplit(term_names_(post_rma_fit), split = sep)) 
+  
+specs <- post_rma_fit$specs 
+
+if(!pretest_name %in% tms) stop("Wrong 'pretest_name='.", call. = FALSE)
+if(!posttest_base_name %in% str_remove(tms, posttest_suffix)) stop("Wrong 'posttest_base_name=' or 'posttest_suffix'.", call. = FALSE)
+
+varis <- if(is_bare_formula(specs, lhs=FALSE)) 
+    .all.vars(specs) else 
+      if(is.character(specs)) specs else stop("Only models with '~moderator1 * moderator2' are acceptable.", call. = FALSE)
+
+if(length(varis) > 2) stop("Only models with TWO interacting variables are acceptable.", call. = FALSE)
+  
+tab <- type.convert(post_rma_fit$table0, as.is=TRUE)[varis]
+
+DATA <- mutate(tab, Variables = apply(tab, 1, paste, collapse="_"),
+               Row = 1:nrow(tab))
+
+gain <- DATA %>%
+  filter(!endsWith(Variables, pretest_name)) %>%
+  mutate(Variables2 = sub(paste0(posttest_base_name,posttest_suffix), pretest_name, Variables),
+         Variables = paste0("(", Variables, " - ", Variables2, ")")) %>%
+  right_join(filter(DATA, endsWith(Variables, pretest_name)), 
+             by = c("Variables2" = "Variables"), suffix = c("_post", "_pre")) %>%
+  group_by(Variables) %>%
+  summarize(Variables, Row = list(c(Row_post, -Row_pre))) %>%
+  tibble::deframe()
+
+if(dif_gain){
+do.call(c, combn(length(gain), 2, 
+                 FUN = function(i)
+                   setNames(list(c(gain[[i[1]]], -gain[[i[2]]])),
+                            paste(names(gain)[i], collapse = " - ")), 
+                 simplify = FALSE))
+} else { gain }
+
+}
 #======================== WCF Meta Dataset ======================================================================================================                
 
 wcf <- read.csv("https://raw.githubusercontent.com/hkil/m/master/wcf.csv")
