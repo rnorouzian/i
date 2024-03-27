93cc167f9c2fa010e1dd40f9572599c3f5ad566b
Reza Norouzian <rnorouzian@gmail.com>
2024-03-27 12:28:58 -0500

    Update 3m.r

--- 3m.r
+++ 3m.r
@@ -2566,24 +2566,32 @@ return(res)
 }
 #================================================================================================================================================
 
-effect_count <- function(data, cluster, ..., arrange_by = NULL){
+effect_count <- function(data, cluster, ..., arrange_by = NULL, show0 = TRUE){
   
-   data <- full_clean(data)
-cluster <- rlang::ensym(cluster)
-cat_mod <- rlang::ensyms(...)
-cat_nms <- purrr::map_chr(cat_mod, rlang::as_string)  
+  data <- full_clean(data)
+  cluster <- rlang::ensym(cluster)
+  cat_mod <- rlang::ensyms(...)
+  cat_nms <- purrr::map_chr(cat_mod, rlang::as_string)  
+  c_nm <- as_string(cluster)
+  clus_nm <- paste("n", c_nm)
+  nms <- c(c_nm, cat_nms)
+  
+  idx <- nms %in% names(data)
+  if(!all(idx)) stop(toString(dQuote(nms[!idx]))," not found in the 'data'.", call. = FALSE)
+  
+dat <-  data %>% 
+    count(!!cluster, !!!rlang::syms(cat_nms)) %>% 
+    group_by(!!!rlang::syms(cat_nms)) %>% 
+    summarize(!!clus_nm := n(),
+              `n effect` = sum(n)) %>% 
+    ungroup() 
 
-idx <- cat_nms %in% names(data)
-if(!all(idx)) stop(toString(dQuote(cat_nms[!idx]))," not found in the 'data'.", call. = FALSE)
   
-data %>% 
-  count(!!cluster, !!!rlang::syms(cat_nms)) %>% 
-  group_by(!!!rlang::syms(cat_nms)) %>% 
-  summarize(`n study` = n(),
-            `n effect` = sum(n)) %>% 
-   ungroup() %>%  
-  tidyr::complete(!!!rlang::syms(cat_nms), fill = list(`n study` = 0, `n effect` = 0)) %>% 
-  arrange(across(if(is.null(arrange_by)) all_of(cat_nms) else all_of(arrange_by)))
+  if(show0) {
+    dat <- dat %>% tidyr::complete(!!!rlang::syms(cat_nms), fill = list2(!!clus_nm := 0, `n effect` = 0))
+}
+
+  dat %>% arrange(across(if(is.null(arrange_by)) all_of(cat_nms) else all_of(arrange_by)))    
 }                            
                             
 #======================== WCF Meta Dataset ======================================================================================================                
