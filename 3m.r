ca817f1235f7006175e7b22950451ab0096a6fb0
Reza Norouzian <rnorouzian@gmail.com>
2025-08-13 18:23:54 -0500

    Update 3m.r

--- 3m.r
+++ 3m.r
@@ -2744,13 +2744,13 @@ effect_count <- function(data, cluster, ..., arrange_by = NULL, show0 = TRUE,
                          na.rm = FALSE, subset){
   
   data <- full_clean(data)
-  
-  if(!missing(subset)) {
-    
-    s <- substitute(subset)
-    data <- filter(data, eval(s))
-  }
-  
+
+if(!missing(subset)) {
+  s <- substitute(subset)
+  data <- filter(data, eval(s, envir = parent.frame()))
+}
+
+ 
   cluster <- rlang::ensym(cluster)
   cat_mod <- rlang::ensyms(...)
   cat_nms <- purrr::map_chr(cat_mod, rlang::as_string)  
